573de2af6e11d59b6b276a75b89fa9a9e13cc89f
Ali Raza <aliatwork@hotmail.co.uk>
2023-03-31 20:49:39 +0100

    v2

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('images'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   //res.send('Hello from App Engine!');
   // point to the index.html file
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
 // add a page for the game and link it to game.html
 app.get('/game', (req, res) => {
-  res.sendFile(path.join(__dirname, 'game.html'));
+  res.sendFile(path.join(__dirname, 'pages/game.html'));
 });
 
-
+// add a page called cloudcore and link it to cloudcore.html which is in the cloudcore folder
+app.get('/cloudcore', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/cloudcore/cloudcore.html'));
+});
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
