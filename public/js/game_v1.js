0f0b34fee5ce6395378b6a233878e6508e174b51
Ali Raza <aliatwork@hotmail.co.uk>
2023-04-04 22:53:07 +0100

    multiplayer accomplished

--- public/js/game_v1.js
+++ public/js/game_v1.js
@@ -1,179 +1,225 @@
+//1
+//This code selects the canvas element using its ID, gets its 2D context, 
+//and initializes the rows, columns, and size of each cell in the grid. 
+//Additionally, it initializes the apple and players variables, 
+//which will be used later in the script.
+
 const cvs = document.getElementById('board');
 const ctx = cvs.getContext('2d');
-const btns = document.getElementById('btns');
-const scoresDiv = document.getElementById('scores');
-
-const size = 20;
-const rows = cvs.height / size;
-const cols = cvs.width / size;
-const colors = ['#0F0', '#F00', '#00F', '#FF0'];
-const controls = [
-  { up: 38, down: 40, left: 37, right: 39 },
-  { up: 87, down: 83, left: 65, right: 68 },
-  { up: 73, down: 75, left: 74, right: 76 },
-  { up: 104, down: 98, left: 100, right: 102 },
-];
-
-let players = [];
+const rows = 30;
+const cols = 30;
+const size = 20; // The size of each cell (20x20)
 let apple;
-let numPlayers;
+let players = [];
 
-function startGame(p) {
-  numPlayers = p;
-  btns.style.display = 'none';
-  init();
-}
+//2
+//The startGame function takes the number of players as an argument and 
+//initializes the players array with new Snake instances for each player. 
+//It also creates a new Apple instance and calls the updateScores function 
+//to display the initial scores. Finally, it starts the game loop by calling 
+//the gameLoop function.
 
-function init() {
-  players = [];
-  for (let i = 0; i < numPlayers; i++) {
-    const x = i % 2 === 0 ? 1 : cols - 2;
-    const y = i < 2 ? 1 : rows - 2;
-    players.push(new Snake(x, y, colors[i], controls[i]));
-  }
-  apple = new Apple();
-  updateScores();
-  gameLoop();
-}
-
-function gameLoop() {
-  ctx.clearRect(0, 0, cvs.width, cvs.height);
-  apple.draw();
-
-  for (const player of players) {
-    player.move();
-    player.draw();
-    player.checkCollision();
-
-    if (player.eat(apple)) {
-      apple = new Apple();
-      player.grow();
-      player.score++;
-      updateScores();
-      if (player.score === 25) {
-        alert(player.color.toUpperCase() + ' player wins!');
-        btns.style.display = 'block';
-        return;
-      }
+//The updateScores function generates the HTML for the scores of all players 
+//and updates the innerHTML of the scores element.
+function startGame(numPlayers) {
+    players = [];
+    for (let i = 0; i < numPlayers; i++) {
+        const startX = i % 2 === 0 ? 1 : cols - 2;
+        const startY = i < 2 ? 1 : rows - 2;
+        players.push(new Snake(startX, startY, i + 1));
     }
-  }
 
-  setTimeout(gameLoop, 100);
+    apple = new Apple();
+    updateScores();
+    gameLoop();
 }
 
 function updateScores() {
-  scoresDiv.innerHTML = '';
-  for (const player of players) {
-    const score = document.createElement('div');
-    score.style.color = player.color;
-    score.innerHTML = player.color.toUpperCase() + ': ' + player.score;
-    scoresDiv.appendChild(score);
-  }
+    let scoresHTML = '';
+    players.forEach((player, index) => {
+        scoresHTML += `<p>Player ${index + 1}: ${player.score}</p>`;
+    });
+    document.getElementById('scores').innerHTML = scoresHTML;
 }
 
+//3
+//The gameLoop function handles the game logic. 
+//It clears the canvas, moves and draws each player's snake, 
+//and draws the apple. Then it checks if any snake has collided with itself, 
+//another snake, or the wall. If so, the snake resets. If a snake eats the 
+//apple, a new apple is created, and the scores are updated. The loop checks 
+//for a winner and, if there's no winner yet, it schedules the next iteration 
+//of the game loop with a timeout.
+function gameLoop() {
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+
+    players.forEach(player => {
+        player.move();
+        player.draw();
+    });
+
+    apple.draw();
+
+    players.forEach(player => {
+        if (player.checkCollision()) {
+            player.reset();
+        } else if (player.eat(apple)) {
+            apple = new Apple();
+            updateScores();
+        }
+    });
+
+    let winner = players.find(player => player.score >= 25);
+    if (winner) {
+        alert(`Player ${winner.id} wins!`);
+    } else {
+        setTimeout(gameLoop, 100);
+    }
+}
+
+//4
+//The Snake class represents a snake in the game. 
+//It has methods for setting controls, checking for 
+//opposite directions, moving, drawing, checking collisions, 
+//resetting, and eating apples. 
 class Snake {
-  constructor(x, y, color, controls) {
-    this.x = x;
-    this.y = y;
-    this.color = color;
-    this.size = size;
-    this.controls = controls;
-    this.dx = 1;
-    this.dy = 0;
-    this.body = [];
-    this.score = 0;
-
-    document.addEventListener('keydown', (e) => {
-      if (e.keyCode === controls.up && this.dy === 0) {
-        this.dx = 0;
-        this.dy = -1;
-      } else if (e.keyCode === controls.down && this.dy === 0) {
-        this.dx = 0;
-        this.dy = 1;
-      } else if (e.keyCode === controls.left && this.dx === 0) {
-        this.dx = -1;
-        this.dy = 0;
-      } else if (e.keyCode === controls.right && this.dx === 0) {
-        this.dx = 1;
-        this.dy = 0;
-      }
-    });
-  }
-
-  move() {
-    this.body.unshift({ x: this.x, y: this.y });
-    this.body.pop();
-    this.x += this.dx;
-    this.y += this.dy;
-  }
-
-  draw() {
-    ctx.fillStyle = this.color;
-    this.body.forEach((p) => {
-      ctx.fillRect(p.x * size, p.y * size, size, size);
-      ctx.strokeRect(p.x * size, p.y * size, size, size);
-    });
-  }
-
-  grow() {
-    this.body.push({});
-  }
-
-  eat(apple) {
-    return this.x === apple.x && this.y === apple.y;
-  }
-
-  checkCollision() {
-    if (this.x < 0 || this.y < 0 || this.x >= cols || this.y >= rows) {
-      this.reset();
+    constructor(x, y, id) {
+        this.body = [{ x, y }];
+        this.id = id;
+        this.color = ['blue', 'green', 'yellow', 'magenta'][id - 1];
+        this.direction = id === 1 ? 'right' : id === 2 ? 'left' : id === 3 ? 'down' : 'up';
+        this.score = 0;
+        this.setControls();
     }
 
-    this.body.forEach((p, i) => {
-      if (i > 0 && this.x === p.x && this.y === p.y) {
-        this.reset();
-      }
-      players.forEach((other) => {
-        if (other !== this && other.body.some((q) => q.x === this.x && q.y === this.y)) {
-          this.reset();
-        }
-      });
-    });
-  }
+    setControls() {
+        document.addEventListener('keydown', e => {
+            const keyMap = {
+                '1': { 37: 'left', 38: 'up', 39: 'right', 40: 'down' },
+                '2': { 65: 'left', 87: 'up', 68: 'right', 83: 'down' },
+                '3': { 100: 'left', 104: 'up', 102: 'right', 101: 'down' },
+                '4': { 72: 'left', 85: 'up', 75: 'right', 74: 'down' },
+            };
+            const newDirection = keyMap[this.id][e.keyCode];
+            if (newDirection && !this.isOppositeDirection(newDirection)) {
+                this.direction = newDirection;
+            }
+        });
+    }
 
-  reset() {
-    this.x = this.controls === controls[0] ? 1 : cols - 2;
-    this.y = this.controls === controls[0] || this.controls === controls[1] ? 1 : rows - 2;
-    this.dx = 1;
-    this.dy = 0;
-    this.body = [];
-    this.score = 0;
-  }
+    isOppositeDirection(newDirection) {
+        const oppositeDirections = {
+            left: 'right',
+            right: 'left',
+            up: 'down',
+            down: 'up',
+        };
+        return this.direction === oppositeDirections[newDirection];
+    }
+
+    //5
+    //These methods handle the movement, drawing, collision detection, 
+    //resetting, and apple eating for each snake. Now the Snake class is complete.
+    move() {
+        let dx = 0;
+        let dy = 0;
+
+        switch (this.direction) {
+            case 'left':
+                dx = -1;
+                break;
+            case 'right':
+                dx = 1;
+                break;
+            case 'up':
+                dy = -1;
+                break;
+            case 'down':
+                dy = 1;
+                break;
+        }
+
+        this.body.unshift({ x: this.body[0].x + dx, y: this.body[0].y + dy });
+        this.body.pop();
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+        this.body.forEach(segment => {
+            ctx.fillRect(segment.x * size, segment.y * size, size, size);
+        });
+    }
+
+    checkCollision() {
+        const head = this.body[0];
+
+        if (head.x < 0 || head.x >= cols || head.y < 0 || head.y >= rows) {
+            return true;
+        }
+
+        for (let i = 1; i < this.body.length; i++) {
+            const segment = this.body[i];
+            if (head.x === segment.x && head.y === segment.y) {
+                return true;
+            }
+        }
+
+        for (const player of players) {
+            if (player.id !== this.id) {
+                for (const segment of player.body) {
+                    if (head.x === segment.x && head.y === segment.y) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    reset() {
+        const startX = this.id % 2 === 0 ? 1 : cols - 2;
+        const startY = this.id < 3 ? 1 : rows - 2;
+        this.body = [{ x: startX, y: startY }];
+        this.score = 0;
+    }
+
+    eat(apple) {
+        const head = this.body[0];
+        if (head.x === apple.x && head.y === apple.y) {
+            this.score++;
+            this.body.push({ x: head.x, y: head.y });
+            return true;
+        }
+        return false;
+    }
+    //These methods handle the movement, drawing, collision detection, 
+    //resetting, and apple eating for each snake. Now the Snake class is complete.
 }
 
+//6
+//The Apple class represents an apple in the game. 
+//The constructor generates random x and y coordinates within the grid, 
+//and sets the color to red. The draw method is responsible for drawing 
+//the apple on the canvas.
 class Apple {
-  constructor() {
-    this.x = Math.floor(Math.random() * cols);
-    this.y = Math.floor(Math.random() * rows);
-    this.color = '#F00';
-    this.size = size;
-
-    for (const player of players) {
-      player.body.forEach((p) => {
-        if (this.x === p.x && this.y === p.y) {
-          this.x = Math.floor(Math.random() * cols);
-          this.y = Math.floor(Math.random() * rows);
-        }
-      });
+    constructor() {
+        this.x = Math.floor(Math.random() * cols);
+        this.y = Math.floor(Math.random() * rows);
+        this.color = 'red';
     }
-  }
 
-  draw() {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x * size, this.y * size, size, size);
-    ctx.strokeRect(this.x * size, this.y * size, size, size);
-  }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x * size, this.y * size, size, size);
+    }
 }
 
-// Resize the canvas to the desired size
-cvs.width = size * cols;
-cvs.height = size * rows;
+function toggleInstructionsPopup() {
+    const popup = document.getElementById("instructions-popup");
+    if (popup.style.display === "none" || popup.style.display === "") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
+}
